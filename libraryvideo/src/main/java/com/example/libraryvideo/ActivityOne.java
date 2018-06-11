ca70d64b73c6859b9134f50a854a559b12a8c4f3
huyang <1184394624@qq.com>
2018-06-12 00:24:02 +0800

    add nkd hello jni in libraryvideo

--- libraryvideo/src/main/java/com/example/libraryvideo/ActivityOne.java
+++ libraryvideo/src/main/java/com/example/libraryvideo/ActivityOne.java
@@ -3,6 +3,7 @@ package com.example.libraryvideo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.example.basecommonlibrary.CommonStation;
@@ -27,8 +28,8 @@ public class ActivityOne extends AppCompatActivity implements View.OnClickListen
         setContentView(R.layout.library_one_activity_main);
         findViewById(R.id.bt1).setOnClickListener(this);
         findViewById(R.id.bt2).setOnClickListener(this);
-        showOperationDialog();
-
+//        showOperationDialog();
+        Toast.makeText(this,stringFromJNI(),Toast.LENGTH_LONG).show();
     }
     private void showOperationDialog(){
         List<String> list = new ArrayList<String>();
@@ -52,5 +53,32 @@ public class ActivityOne extends AppCompatActivity implements View.OnClickListen
         } else if (i == R.id.bt2) {
             RouterCommonUtil.startLibraryTwoActivity(this);
         }
+    }
+
+    /* A native method that is implemented by the
+     * 'hello-jni' native library, which is packaged
+     * with this application.
+     */
+    public native String  stringFromJNI();
+
+    /* This is another native method declaration that is *not*
+     * implemented by 'hello-jni'. This is simply to show that
+     * you can declare as many native methods in your Java code
+     * as you want, their implementation is searched in the
+     * currently loaded native libraries only the first time
+     * you call them.
+     *
+     * Trying to call this function will result in a
+     * java.lang.UnsatisfiedLinkError exception !
+     */
+    public native String  unimplementedStringFromJNI();
+
+    /* this is used to load the 'hello-jni' library on application
+     * startup. The library has already been unpacked into
+     * /data/data/com.example.hellojni/lib/libhello-jni.so at
+     * installation time by the package manager.
+     */
+    static {
+        System.loadLibrary("jni-lib");
     }
 }
