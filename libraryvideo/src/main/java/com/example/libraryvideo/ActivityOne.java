292b42aa7fcbb18644cf9c289182569fbf52764c
huyang <1184394624@qq.com>
2018-06-14 22:48:39 +0800

    add opengl in libraryone1

--- libraryvideo/src/main/java/com/example/libraryvideo/ActivityOne.java
+++ libraryvideo/src/main/java/com/example/libraryvideo/ActivityOne.java
@@ -6,6 +6,7 @@ import android.content.pm.PackageManager;
 import android.opengl.EGLConfig;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
@@ -16,6 +17,8 @@ import android.widget.Toast;
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.example.basecommonlibrary.CommonStation;
 import com.example.basecommonlibrary.RouterCommonUtil;
+import com.example.libraryvideo.shape.Square;
+import com.example.libraryvideo.shape.Triangle;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -154,19 +157,48 @@ public class ActivityOne extends AppCompatActivity implements View.OnClickListen
 
     public class MyGLRenderer implements GLSurfaceView.Renderer {
 
+        private Triangle mTriangle;
+        private Square mSquare;
+
+        // mMVPMatrix is an abbreviation for "Model View Projection Matrix"
+        private final float[] mMVPMatrix = new float[16];
+        private final float[] mProjectionMatrix = new float[16];
+        private final float[] mViewMatrix = new float[16];
+
         public void onDrawFrame(GL10 unused) {
             // Redraw background color
             GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+//            mTriangle.draw();
+
+            // Set the camera position (View matrix)
+            Matrix.setLookAtM(mViewMatrix, 0, 0, 0, -3, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
+
+            // Calculate the projection and view transformation
+            Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0);
+
+            // Draw shape
+            mTriangle.draw(mMVPMatrix);
         }
 
         @Override
         public void onSurfaceCreated(GL10 gl, javax.microedition.khronos.egl.EGLConfig config) {
             // Set the background frame color
-            GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+            GLES20.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+            // initialize a triangle
+            mTriangle = new Triangle();
+            // initialize a square
+            mSquare = new Square();
         }
 
         public void onSurfaceChanged(GL10 unused, int width, int height) {
             GLES20.glViewport(0, 0, width, height);
+            float ratio = (float) width / height;
+
+            // this projection matrix is applied to object coordinates
+            // in the onDrawFrame() method
+            Matrix.frustumM(mProjectionMatrix, 0, -ratio, ratio, -1, 1, 3, 7);
+
         }
     }
 }
