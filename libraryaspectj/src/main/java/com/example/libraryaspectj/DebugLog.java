873b0c94fcccf32d3154682c73eed280dadb04d1
huyang <1184394624@qq.com>
2018-06-19 18:19:45 +0800

    add aspectj in libraryaspectj

---
+++ libraryaspectj/src/main/java/com/example/libraryaspectj/DebugLog.java
@@ -0,0 +1,17 @@
+package com.example.libraryaspectj;
+
+import android.util.Log;
+
+public class DebugLog {
+    private DebugLog() {}
+
+    /**
+     * Send a debug log message
+     *
+     * @param tag Source of a log message.
+     * @param message The message you would like logged.
+     */
+    public static void log(String tag, String message) {
+        Log.d(tag, message);
+    }
+}
