763683f146c75610240ea2142036454563b42c63
huyang <1184394624@qq.com>
2018-07-11 02:27:07 +0800

    merge

--- libraryplugin/src/main/groovy/com/example/JavassistPlugin.groovy
+++ libraryplugin/src/main/groovy/com/example/JavassistPlugin.groovy
@@ -6,6 +6,9 @@ import org.gradle.api.Project
 public class JavassistPlugin implements Plugin<Project> {
 
     void apply(Project project) {
+        project.dependencies {
+            compile 'org.aspectj:aspectjrt:1.8.9'
+        }
         def log = project.logger
         log.error "========================";
         log.error "Javassist开始修改Class!";
--- libraryplugin/src/main/groovy/com/example/JavassistTransform.groovy
+++ libraryplugin/src/main/groovy/com/example/JavassistTransform.groovy
@@ -49,18 +49,18 @@ public class JavassistTransform extends Transform {
         inputs.each { TransformInput input ->
             try {
                 input.jarInputs.each {
-                    MyInject.injectDir(it.file.getAbsolutePath(), "com", project)
+                    TimeInject.injectDir(it.file.getAbsolutePath(), "com", project)
                     String outputFileName = it.name.replace(".jar", "") + '-' + it.file.path.hashCode()
                     def output = outputProvider.getContentLocation(outputFileName, it.contentTypes, it.scopes, Format.JAR)
                     FileUtils.copyFile(it.file, output)
                 }
             } catch (Exception e) {
-                project.logger.err e.getMessage()
+//                project.logger.err e.getMessage()
             }
             //对类型为“文件夹”的input进行遍历
             input.directoryInputs.each { DirectoryInput directoryInput ->
                 //文件夹里面包含的是我们手写的类以及R.class、BuildConfig.class以及R$XXX.class等
-                MyInject.injectDir(directoryInput.file.absolutePath, "com", project)
+                TimeInject.injectDir(directoryInput.file.absolutePath, "com", project)
                 // 获取output目录
                 def dest = outputProvider.getContentLocation(directoryInput.name,
                         directoryInput.contentTypes, directoryInput.scopes,
@@ -71,6 +71,6 @@ public class JavassistTransform extends Transform {
             }
         }
         ClassPool.getDefault().clearImportedPackages();
-        project.logger.error("JavassistTransform cast :" + (System.currentTimeMillis() - startTime) / 1000 + " secs");
+//        project.logger.error("*** yang","JavassistTransform cast :" + (System.currentTimeMillis() - startTime) / 1000 + " secs");
     }
 }
