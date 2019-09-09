84a840ab0fd4c9dd0fa0769c3be2128e53e91450
huyang <1184394624@qq.com>
2019-09-10 00:24:11 +0800

    opengles  动态旋转视频

--- opengl/src/main/java/com/hy/opengl/GLVideoRenderer.java
+++ opengl/src/main/java/com/hy/opengl/GLVideoRenderer.java
@@ -9,6 +9,7 @@ import android.opengl.GLES11Ext;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 import android.opengl.Matrix;
+import android.os.SystemClock;
 import android.util.Log;
 import android.view.Surface;
 
@@ -88,22 +89,6 @@ public class GLVideoRenderer  implements GLSurfaceView.Renderer
             "    gl_Position = uMatrix*aPosition;\n" +
             "}";
 
-    String fragmentShader1 = "" +
-            "#extension GL_OES_EGL_image_external : require\n" +
-            "precision mediump float;\n" +
-            "varying vec2 vTexCoord;\n" +
-            "uniform samplerExternalOES sTexture;\n" +
-            "void main() {\n" +
-            "\n" +
-            "      //  vec3 centralColor = texture2D(sTexture, vTexCoord).rgb;\n" +
-            "     //   gl_FragColor = vec4(0.299*centralColor.r+0.587*centralColor.g+0.114*centralColor.b);\n" +
-            "       //            vec4 src = texture2D(sTexture, vTexCoord);\n" +
-            "       //             gl_FragColor = vec4(1.0 - src.r, 1.0 - src.g, 1.0 - src.b, 1.0);" +
-            "        gl_FragColor=texture2D(sTexture, vTexCoord * cloneCount);\n" +
-            "    //   gl_FragColor=texture2D(sTexture, vTexCoord );\n" +
-            "\n" +
-            "}";
-
     private final String fragmentShader =" " +
             "#extension GL_OES_EGL_image_external : require\n" +
             " precision mediump float;\n" +
@@ -173,7 +158,7 @@ public class GLVideoRenderer  implements GLSurfaceView.Renderer
             e.printStackTrace();
         }
         mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-        mediaPlayer.setLooping(false);
+        mediaPlayer.setLooping(true);
         mediaPlayer.setOnVideoSizeChangedListener(this);
     }
 
@@ -191,6 +176,13 @@ public class GLVideoRenderer  implements GLSurfaceView.Renderer
         });
     }
     private long startTime = 0;
+
+    private float[] mViewMatrix=new float[16];
+    private float[] mProjectMatrix=new float[16];
+    private float[] mMVPMatrix=new float[16];
+    private float[] mRotationMatrix = new float[16];
+    private int mMVPMatrixHandle;
+
     @Override
     public void onDrawFrame(GL10 gl) {
         GLES20.glClear(GLES20.GL_DEPTH_BUFFER_BIT | GLES20.GL_COLOR_BUFFER_BIT);
@@ -201,12 +193,33 @@ public class GLVideoRenderer  implements GLSurfaceView.Renderer
                 updateSurface = false;
             }
         }
+
+
+
         GLES20.glUseProgram(programId);
-        GLES20.glUniformMatrix4fv(uMatrixLocation, 1, false, projectionMatrix, 0);
+//
+        long time = SystemClock.uptimeMillis() % 4000L;
+        float angle = 0.090f * ((int) time);
+        float[] scratch = new float[16];
+        Matrix.setLookAtM(mViewMatrix, 0, 0, 0, -3, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
+
+        // 计算绘制物体最终在屏幕上的投影和视图变换
+        Matrix.multiplyMM(mMVPMatrix, 0, projectionMatrix, 0, mViewMatrix, 0);
+        Matrix.setRotateM(mRotationMatrix, 0, angle, 0, 0, -1.0f);
+        Matrix.multiplyMM(scratch, 0, mMVPMatrix, 0, mRotationMatrix, 0);
+
+        // 将投影和视图变换传递给渲染器
+        GLES20.glUniformMatrix4fv(uMatrixLocation, 1, false, scratch, 0);
+
+//        GLES20.glUniformMatrix4fv(uMatrixLocation, 1, false, projectionMatrix, 0);
         GLES20.glUniformMatrix4fv(uSTMMatrixHandle, 1, false, mSTMatrix, 0);
         double intensity = Math.abs(Math.sin((System.currentTimeMillis() - startTime) / 1000.0)) + 0.5;
         Log.d(TAG, "onDrawFrame() called with: intensity = [" + intensity + "]");
-        GLES20.glUniform1f(intensityLocation, (float) intensity);
+        GLES20.glUniform1f(intensityLocation, (float)intensity);
+
+
+
+
 
         vertexBuffer.position(0);
         GLES20.glEnableVertexAttribArray(aPositionLocation);
@@ -214,6 +227,7 @@ public class GLVideoRenderer  implements GLSurfaceView.Renderer
                 12, vertexBuffer);
 
         textureVertexBuffer.position(0);
+
         GLES20.glEnableVertexAttribArray(aTextureCoordLocation);
         GLES20.glVertexAttribPointer(aTextureCoordLocation, 2, GLES20.GL_FLOAT, false, 8, textureVertexBuffer);
 
@@ -235,15 +249,30 @@ public class GLVideoRenderer  implements GLSurfaceView.Renderer
         updateProjection(width, height);
     }
 
-    private void updateProjection(int videoWidth, int videoHeight) {
+    private void updateProjection(int width, int height) {
         float screenRatio = (float) screenWidth / screenHeight;
-        float videoRatio = (float) videoWidth / videoHeight;
+        float videoRatio = (float) width / height;
+//        if (videoRatio > screenRatio) {
+//            Matrix.orthoM(projectionMatrix, 0, -1f, 1f, -videoRatio / screenRatio, videoRatio / screenRatio, -1f, 1f);
+//        } else{
+//            Matrix.orthoM(projectionMatrix, 0, -screenRatio / videoRatio, screenRatio / videoRatio, -1f, 1f, -1f, 1f);
+//
+//        }
+        GLES20.glViewport(0, 0, width, height);
+
+        float ratio = (float) width / height;
+
         if (videoRatio > screenRatio) {
-            Matrix.orthoM(projectionMatrix, 0, -1f, 1f, -videoRatio / screenRatio, videoRatio / screenRatio, -1f, 1f);
+//            Matrix.orthoM(projectionMatrix, 0, -1f, 1f, -videoRatio / screenRatio, videoRatio / screenRatio, -1f, 1f);
+            Matrix.frustumM(projectionMatrix, 0,-ratio, ratio, -1, 1, 3, 7);
         } else{
-            Matrix.orthoM(projectionMatrix, 0, -screenRatio / videoRatio, screenRatio / videoRatio, -1f, 1f, -1f, 1f);
+            Matrix.frustumM(projectionMatrix, 0,ratio, -ratio, -1, 1, 3, 7);
+//            Matrix.orthoM(projectionMatrix, 0, -screenRatio / videoRatio, screenRatio / videoRatio, -1f, 1f, -1f, 1f);
 
         }
+
+        // 这个投影矩阵被用于onDrawFrame()中绘制对象的坐标系
+//        Matrix.frustumM(projectionMatrix, 0, -ratio, ratio, -1, 1, 3, 7);
     }
 
     public MediaPlayer getMediaPlayer() {
